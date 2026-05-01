87c3590c4f657e97a3a94db709654cb7fe0ca2c9
Dnyaneshwari18594 <dnyaneshwaritambe18@gmail.com>
2026-05-01 17:07:21 +0530

    lets deploy

---
+++ api/main.py
@@ -0,0 +1,20 @@
+## backend code 
+
+from fastapi import FastAPI
+from app.schemas import WeatherInput
+from app.predictor import predict_weather
+
+
+app = FastAPI(title="Thunderstorm Prediction API")
+
+# to ensure app is running
+@app.get("/")
+def home():
+    return {"message": "Weather Prediction API is running"}
+
+# microservice
+@app.post("/predict")
+def predict(data: WeatherInput):
+    features = data.to_list()
+    result = predict_weather(features)
+    return result
---
+++ app/config.py
@@ -0,0 +1,1 @@
+MODEL_PATH = "experiments/models/best_model.pkl"
